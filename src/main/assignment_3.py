0a2572483f953dc82e454f62592058b57efd64da
HyrumVonn <uberhimer@gmail.com>
2025-04-02 19:44:29 -0400

    Lay out final program setup

---
+++ src/main/assignment_3.py
@@ -0,0 +1,39 @@
+#Hyrum VonNiederhausern
+
+def GaussianElimination(matrix):
+    return 0
+
+def LUFactorization(matrix):
+    return 0
+
+def DiagonallyDominant(matrix):
+    return True
+
+def PositiveDefinite(matrix):
+    return True
+
+
+GaussianElimination([[2,-1,1,6],
+                     [1,3,1,0],
+                     [-1,5,4,-3]])
+
+print()
+
+LUFactorization([[1,1,0,3],
+                 [2,1,-1,1],
+                 [3,-1,-1,2],
+                 [-1,2,3,-1]])
+
+print()
+
+DiagonallyDominant([[9,0,5,2,1],
+                    [3,9,1,2,1],
+                    [0,1,7,2,3],
+                    [4,2,3,12,2],
+                    [3,2,4,0,8]])
+
+print()
+
+PositiveDefinite([[2,2,1],
+                  [2,3,0],
+                  [1,0,2]])
